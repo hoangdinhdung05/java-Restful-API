c24d2ab9aa61adbbd3927c9bf703e6c7c86d844a
hoangdinhdung05 <hoangdinhdung0205@gmail.com>
2025-06-08 00:10:27 +0700

    Cors với FilterRegistrationBean

--- src/main/java/vn/hoangdung/config/AppConfig.java
+++ src/main/java/vn/hoangdung/config/AppConfig.java
@@ -2,23 +2,42 @@ package vn.hoangdung.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class AppConfig {
 
+//    @Bean
+//    public WebMvcConfigurer corsFilter() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowCredentials(true)
+//                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*");
+//            }
+//        };
+//    }
+
     @Bean
-    public WebMvcConfigurer corsFilter() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowCredentials(true)
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://127.0.0.1:5500");
+        config.addAllowedOrigin("http://localhost:5500"); // thêm nếu dùng localhost
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); // Cho phép tất cả các method HTTP
+
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
+
 }
